22d29d0b3dccc944be673968fd79600ed377547b
charliegregg <charliejamesgregg@gmail.com>
2024-03-02 23:23:25 +1100

    level and cam

---
+++ scripts/cam.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+@export var player: CharacterBody2D
+
+var adjust_y: bool = false
+
+func _process(delta):
+	position.x = lerp(position.x, player.position.x, 0.04)
+	if abs(position.y - player.position.y) > 200:
+		adjust_y = true
+	if adjust_y:
+		position.y = lerp(position.y, player.position.y, 0.04)
+	if abs(position.y - player.position.y) < 20:
+		adjust_y = false
