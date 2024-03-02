2ed4b1ede517b77074d7cbc6c8c80c7100b70733
charliegregg <charliejamesgregg@gmail.com>
2024-03-02 23:34:20 +1100

    flag patch

--- scripts/flag.gd
+++ scripts/flag.gd
@@ -3,4 +3,5 @@ extends Area2D
 @export var next_level: PackedScene
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_packed(next_level)
+	if body is Player:
+		get_tree().change_scene_to_packed(next_level)
